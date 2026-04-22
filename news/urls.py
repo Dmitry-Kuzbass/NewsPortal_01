0681cd2354adf17c243cb8300ecd23a9ec11e52c
Dmitry_Kuzbass <edv123@rambler.ru>
2026-04-22 09:02:34 +0700

    Добавил изменения фильтр cesor

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # Путь для списка новостей: /news/
     path('', NewsList.as_view(), name='news_list'),
     # Путь для одной новости: /news/<id>
-    path('<int:pk>', NewsDetail.as_view(), name='news_detail'),
+    path('<int:pk>/', NewsDetail.as_view(), name='news_detail'),
 ]
